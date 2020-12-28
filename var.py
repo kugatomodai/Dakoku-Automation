62273c2907c2805d8714f71861d918e51311fe1a
kugatomodai <41180719+kugatomodai@users.noreply.github.com>
2020-12-28 18:43:51 +0900

    Create var.py
    変数をまとめてここに記述

---
+++ var.py
@@ -0,0 +1,13 @@
+"""
+ユーザ名・パスワード・出退勤管理web page URL、chromedriverのパスをこちらのファイルに記述
+"""
+
+# Chromedriverの絶対パスに置き換えてください
+driver_path = "<location of your chromedriver>"
+
+# 出退勤管理用のユーザ名・パスワードに置き換えてください
+account_user = "<your user name here>"
+account_pass = "<your password here>"
+
+# 出退勤管理webページのURLに置き換えてください
+page = "<your URL here>"
